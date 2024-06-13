eebd43e85636595a5082ee3ba83867346ea6cf3e
Vito Andareas Manik <vitoandareas15@gmail.com>
2024-06-13 14:07:07 +0800

    feat: integrating auth with clerk

--- common/models/Schema.ts
+++ common/models/Schema.ts
@@ -1,15 +1,20 @@
-import { pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { uuidv7 } from "uuidv7";
 
 const RoleEnum = pgEnum("role", ["teacher", "student"]);
+const createdAt = timestamp("created_at", { mode: "string" }).defaultNow();
+const updatedAt = timestamp("updated_at", { mode: "string" }).defaultNow();
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
+  id: text("user_id")
+    .$defaultFn(() => uuidv7())
+    .primaryKey(),
   username: text("username"),
   email: text("email").unique(),
   passwordHash: text("password_hash"),
   role: RoleEnum("role"),
   profilePicture: text("profile_picture"),
   bio: text("bio"),
-  createdAt: timestamp("created_at", { mode: "string" }).defaultNow(),
-  updatedAt: timestamp("updated_at", { mode: "string" }).defaultNow(),
+  createdAt,
+  updatedAt,
 });
