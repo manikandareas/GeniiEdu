4cc5ff2c81397404e9842268531d7af952653927
Vito Andareas Manik <vitoandareas15@gmail.com>
2024-06-13 17:21:54 +0800

    feat: adding database schema migrations

--- common/libs/DB.ts
+++ common/libs/DB.ts
@@ -1,13 +1,13 @@
-import * as schema from "@/common/models/Schema";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import { Env } from "./Env";
+import * as schema from '@/common/models/Schema';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { Env } from './Env';
 
 const pool = new Pool({
-  host: Env.DATABASE_HOST as string,
-  port: Env.DATABASE_PORT as number,
-  user: Env.DATABASE_USER as string,
-  database: Env.DATABASE_NAME as string,
+    host: Env.DATABASE_HOST as string,
+    port: Env.DATABASE_PORT as number,
+    user: Env.DATABASE_USER as string,
+    database: Env.DATABASE_NAME as string,
 });
 
 export const db = drizzle(pool, { schema, logger: true });
